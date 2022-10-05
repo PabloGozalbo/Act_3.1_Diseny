966b03d1c2f57af955967ff145e9a9e2f58a109f
Pablo Gozalbo Solsona <al394763@uji.es>
2022-10-05 10:54:32 +0200

    EJ 4

--- src/DemoSingelton.java
+++ src/DemoSingelton.java
@@ -1,8 +1,12 @@
 public class DemoSingelton {
     public static void main(String[] args) {
+        System.out.println(System.currentTimeMillis());
         Singelton instance1 = Singelton.getInstance();
+        System.out.println(System.currentTimeMillis());
         System.out.println(instance1);
+        System.out.println(System.currentTimeMillis());
         Singelton instance2 = Singelton.getInstance();
+        System.out.println(System.currentTimeMillis());
         System.out.println(instance2);
     }
 }
